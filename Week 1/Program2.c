acd3367ebe667e711912d5ea045f340ab1e52555
ri5hu <69378555+ri5hu@users.noreply.github.com>
2020-08-16 22:07:48 +0530

    Update Program2.c

--- Week 1/Program2.c
+++ Week 1/Program2.c
@@ -1,3 +1,6 @@
+/*Implementation of Language recognizer for set of all strings over input alphabet ∑={a,b}
+containing even number of a’s and even number of b’s.*/
+
 #include <stdio.h>
 
 int main()
